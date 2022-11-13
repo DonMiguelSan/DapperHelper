61ebad201f4c5d2ea10202298e289349198189f9
DonMiguelSan <m.sanchez1@outlook.es>
2022-11-13 14:18:59 +0100

    Config manager helper class added to manage app configs

---
+++ DapperHelper/Tools/ConfigManagerHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperHelper.Tools
+{
+    public static class ConfigManagerHelper
+    {
+        /// <summary>
+        /// Gets the connections string from the App.config file
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns>
+        /// A <see cref="string"/> containing the stored connection string from App.config
+        /// </returns>
+        public static string GetConnectionString(string name)
+        {
+            return ConfigurationManager.ConnectionStrings[name].ConnectionString + ";" +
+                "Integrated Security = SSPI;" + "MultipleActiveResultSets = True;";
+        }
+
+    }
+}
