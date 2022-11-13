2571e240b12858758498fc32acc4e2fce8fd0b70
DonMiguelSan <m.sanchez1@outlook.es>
2022-11-13 14:20:51 +0100

    Dynamic mapper to map dynamically collumn names with spaces to a class model, supports async queries

--- DapperHelper/Tools/DynamicMapper.cs
+++ DapperHelper/Tools/DynamicMapper.cs
@@ -1,52 +1,72 @@
 ﻿
 using Dapper;
 using DapperHelper.Attributes;
-using DapperHelper.Tools.ExtensionMethods;
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Reflection;
-using System.Text;
 using System.Threading.Tasks;
 using System.Web.Routing;
 
 namespace DapperHelper.Tools
 {
-    public class DynamicMapper<T> :IDisposable where  T : class, new()
+    public class DynamicMapper<T> : IDisposable where T : class, new()
     {
-        private readonly Dictionary<string, PropertyInfo> _propertiesMap;
+        private Dictionary<string, PropertyInfo> _propertiesMap;
+
+        private IEnumerable<dynamic> queryResult;
+
+        private readonly IDbConnection dbConnection;
+
         public DynamicMapper()
         {
-            _propertiesMap = new Dictionary<string, PropertyInfo>();
-
-            PropertyInfo[] propertyInfos = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
-
-            foreach (PropertyInfo propertyInfo in propertyInfos)
-            {
-                if (propertyInfo.GetCustomAttribute(typeof(ColumNameAttribute)) is ColumNameAttribute columNameAttribute)
-                {
-                    _propertiesMap.Add(columNameAttribute.ColumnName, propertyInfo);
-                }
-                else
-                {
-                    _propertiesMap.Add(propertyInfo.Name, propertyInfo);
-                }
-            }
+            GeneratePropsMap();
+        }
+        public DynamicMapper(string conStringName) : this()
+        {
+            dbConnection = new SqlConnection(ConfigManagerHelper.GetConnectionString(conStringName));
         }
 
-        public List<T> QueryDynamic(IDbConnection dbConnection, string sqlQuery)
+        public List<T> QueryDynamic(string sqlQuery, int? Timeout = 1000, IDbConnection dbConnection = null)
         {
-            List<dynamic> results = dbConnection.Query(sqlQuery).ToList();
-
-            List<T> output = new List<T>();
-
-            foreach (dynamic dynObj in results)
+            var con = dbConnection ?? this.dbConnection;
+            try
             {
-                output.Add(AssignPropertyValues(dynObj));
+                con.Open();
+
+                queryResult = con.Query(sqlQuery, commandTimeout: Timeout);
+
+                con.Close();
+
+            }
+            catch (Exception)
+            {
+                con?.Close();
             }
 
-            return output;
+            return MapResult();
+        }
+
+        public async Task<List<T>> QueryDynamicAsync(string sqlQuery, int? Timeout = 1000, IDbConnection dbConnection = null)
+        {
+            var con = dbConnection ?? this.dbConnection;
+
+            try
+            {
+                con.Open();
+
+                queryResult = await con.QueryAsync(sqlQuery, commandTimeout: Timeout);
+
+                con.Close();
+            }
+            catch (Exception)
+            {
+                con.Close();
+            }
+
+            return MapResult();
         }
 
         private T AssignPropertyValues(dynamic dynamicObject)
@@ -67,10 +87,42 @@ namespace DapperHelper.Tools
             return output;
         }
 
+        private void GeneratePropsMap()
+        {
+            _propertiesMap = new Dictionary<string, PropertyInfo>();
+
+            PropertyInfo[] propertyInfos = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+            foreach (PropertyInfo propertyInfo in propertyInfos)
+            {
+                if (propertyInfo.GetCustomAttribute(typeof(ColumnNameAttribute)) is ColumnNameAttribute columNameAttribute)
+                {
+                    _propertiesMap.Add(columNameAttribute.ColumnName, propertyInfo);
+                }
+                else
+                {
+                    _propertiesMap.Add(propertyInfo.Name, propertyInfo);
+                }
+            }
+        }
+
+        private List<T> MapResult()
+        {
+            List<T> output = new List<T>();
+
+            foreach (dynamic dynObj in queryResult.ToList())
+            {
+                output.Add(AssignPropertyValues(dynObj));
+            }
+
+            return output;
+        }
 
         public void Dispose()
         {
-            _propertiesMap.Clear(); 
+            _propertiesMap.Clear();
+
+            dbConnection?.Close();
         }
     }
 }
